bc7a07ae5bf53b9c144559b9588aecea12746a92
Vivekanand Swamy Koppula <Vivekanand.Swamy@gmail.com>
2024-02-09 18:30:31 +0530

    Added more info

--- src/apps/401720-CompoundOperators_ReusingOperators/main.cpp
+++ src/apps/401720-CompoundOperators_ReusingOperators/main.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 #include "point.h"
 
-int main(){
+int main()
+{
 
+    Point p1(10, 10);
+    Point p2(20, 20);
+    Point p3(5, 5);
 
-    Point p1(10,10);
-    Point p2(20,20);
-    Point p3(5,5);
+    std::cout << "We have got 3 points p1, p2, and p3 as follows. " << std::endl;
+    std::cout << "p1: " << p1 << std::endl; // (10,10)
+    std::cout << "p2: " << p2 << std::endl; // (20,20)
+    std::cout << "p3: " << p3 << std::endl; // (5,5)
 
-    std::cout << "p1 + p2 : " << (p1 +  p2) << std::endl; // (30,30)
+    std::cout << " " << std::endl;
+
+    std::cout << "p1 + p2 : " << (p1 + p2) << std::endl; // (30,30)
     std::cout << "p2 - p3 : " << (p2 - p3) << std::endl; // (15,15)
 
-    std::cout << "------" << std::endl;
+    std::cout << "" << std::endl;
 
-    p1+=p2;
-    p2-=p3;
-    
-    std::cout << "point1 : " <<  p1 << std::endl; // (30,30)
+    p1 += p2;
+    p2 -= p3;
+
+    std::cout << "points p1 after the operation p1 += p2; " << std::endl;
+    std::cout << "point1 : " << p1 << std::endl; // (30,30)
+
+    std::cout << " " << std::endl;
+
+    std::cout << "points p2 after the operation p2 -= p3; " << std::endl;
     std::cout << "point2 : " << p2 << std::endl; // (15,15)
 
     return 0;
--- src/tasks/401720-CompoundOperators_ReusingOperators/ReadMe.md
+++ src/tasks/401720-CompoundOperators_ReusingOperators/ReadMe.md
@@ -1,7 +1,22 @@
 # Auto
 
 ## Notes
-1. 
+1. First we are defining the **+=** for the point class. This means we could some thing like the following.
+
+```cpp
+p1+=p2;
+```
+
+2. Basically we are adding p2 to p1. 
+
+3. Then we are defining **operator+** as follows. So we are duing the **+=** defined above.
+
+```cpp
+inline Point operator+(const Point& left , const Point& right){
+	Point p(left);
+	return p+=right;
+}
+```
 
 
 ## References
