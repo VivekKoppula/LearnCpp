be2ac8326731e1fdb700cb773573e24cc2da1832
Vivekanand Swamy Koppula <Vivekanand.Swamy@gmail.com>
2024-02-10 20:03:48 +0530

    Added more info

--- src/apps/402005-CopyAssignmentOperator/point.cpp
+++ src/apps/402005-CopyAssignmentOperator/point.cpp
@@ -1,23 +1,19 @@
 #include <cmath>
 #include "point.h"
 
-
-double Point::length() const{
-    return sqrt(pow(m_x - 0, 2) +  pow(m_y - 0, 2) * 1.0); 
+double Point::length() const
+{
+    return sqrt(pow(m_x - 0, 2) + pow(m_y - 0, 2) * 1.0);
 }
 
 Point::Point(const Point &p)
 {
     std::cout << "Copy constructor called to copy point " << p << std::endl;
-    if(this != &p){
-		delete p_data;
-		p_data = new int(*(p.p_data));
+    if (this != &p)
+    {
+        // delete p_data; // This line is causing some problem.
+        p_data = new int(*(p.p_data));
         m_x = p.m_x;
         m_y = p.m_y;
     }
 }
-
-
-
-
-
