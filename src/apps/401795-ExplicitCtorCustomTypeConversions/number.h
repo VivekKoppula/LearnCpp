ac4b3fce9c71ebb6ca5bdc64ce368d10a234ff16
Vivekanand Swamy Koppula <Vivekanand.Swamy@gmail.com>
2024-02-10 12:05:01 +0530

    Added more info

--- src/apps/401795-ExplicitCtorCustomTypeConversions/number.h
+++ src/apps/401795-ExplicitCtorCustomTypeConversions/number.h
@@ -3,34 +3,34 @@
 
 class Number
 {
-    friend std::ostream& operator<<(std::ostream& out , const Number& number);
+    friend std::ostream &operator<<(std::ostream &out, const Number &number);
 
-    //Arithmetic operators
-    friend Number operator+(const Number& left_operand, const Number& right_operand);
-		
+    // Arithmetic operators
+    friend Number operator+(const Number &left_operand, const Number &right_operand);
+
 public:
-	Number() = default;
-	explicit Number(int value );
+    Number() = default;
+    explicit Number(int value);
 
+    explicit operator double() const
+    {
+        return (static_cast<double>(m_wrapped_int));
+    }
 
-    // explicit operator double()const{
-    //     return (static_cast <double> (m_wrapped_int));
-    // }
-
-    /*
-    //Implicit conversions won't work for the first operand
-    Number operator+(const Number& right) const{
+    // Implicit conversions won't work for the first operand
+    Number operator+(const Number &right) const
+    {
         return Number(m_wrapped_int + right.m_wrapped_int);
     }
-    */
- 
-    //getter
-    int get_wrapped_int() const{
+
+    // getter
+    int get_wrapped_int() const
+    {
         return m_wrapped_int;
     }
-     
+
     ~Number();
-    
-private : 
+
+private:
     int m_wrapped_int{0};
 };
--- src/tasks/401795-ExplicitCtorCustomTypeConversions/ReadMe.md
+++ src/tasks/401795-ExplicitCtorCustomTypeConversions/ReadMe.md
@@ -27,6 +27,28 @@ Then it will result in a compiler error.
 no known conversion for argument 2 from 'int' to 'const Number&'
 ```
 
+You can add an implicit operetor as follows, then it will work.
+
+```cpp
+operator double()const{
+    return (static_cast <double> (m_wrapped_int));
+}
+```
+
+Now try making this operator also explicit. So what we now have is both the ctor and the operator explicit as follows. Then again it WILL NOT work.
+
+```cpp
+explicit Number(int value);
+
+explicit operator double()const{
+    return (static_cast <double> (m_wrapped_int));
+}
+```
+
+So to make it work, make one of those two into implicit. Then it will work.
+
+
+
 ## References
 
 1. 
