cd0895db8352b391297eff3e96166a617448d911
Vivekanand Swamy Koppula <Vivekanand.Swamy@gmail.com>
2024-03-02 10:53:57 +0530

    Added more info

--- src/apps/404760-LogicalOperatorsSimplified/number.h
+++ src/apps/404760-LogicalOperatorsSimplified/number.h
@@ -4,61 +4,63 @@
 #include <iostream>
 class Number
 {
-	
-    friend std::ostream& operator<<(std::ostream& out , const Number& number);
-    
-    //Comparison operators
-    /*
-    friend bool operator<(const Number& left_operand, const Number& right_operand);
-    friend bool operator<(int left_operand, const Number& right_operand);
-    friend bool operator<(const Number& left_operand, int right_operand);
 
-    friend bool operator==(const Number& left_operand, const Number& right_operand);
-    friend bool operator==(int left_operand, const Number& right_operand);
-    friend bool operator==(const Number& left_operand, int right_operand);
+    friend std::ostream &operator<<(std::ostream &out, const Number &number);
 
-    friend bool operator>(const Number& left_operand, const Number& right_operand);
-    friend bool operator>(int left_operand, const Number& right_operand);
-    friend bool operator>(const Number& left_operand, int right_operand);
+    // Comparison operators
 
-    friend bool operator>=(const Number& left_operand, const Number& right_operand);
-    friend bool operator>=(int left_operand, const Number& right_operand);
-    friend bool operator>=(const Number& left_operand, int right_operand);
+    friend bool operator<(const Number &left_operand, const Number &right_operand);
+    friend bool operator<(int left_operand, const Number &right_operand);
+    friend bool operator<(const Number &left_operand, int right_operand);
 
-    friend bool operator<=(const Number& left_operand, const Number& right_operand);
-    friend bool operator<=(int left_operand, const Number& right_operand);
-    friend bool operator<=(const Number& left_operand, int right_operand);
+    friend bool operator==(const Number &left_operand, const Number &right_operand);
+    friend bool operator==(int left_operand, const Number &right_operand);
+    friend bool operator==(const Number &left_operand, int right_operand);
 
-    friend bool operator!=(const Number& left_operand, const Number& right_operand);
-    friend bool operator!=(int left_operand, const Number& right_operand);
-    friend bool operator!=(const Number& left_operand, int right_operand);
-    */
+    friend bool operator>(const Number &left_operand, const Number &right_operand);
+    friend bool operator>(int left_operand, const Number &right_operand);
+    friend bool operator>(const Number &left_operand, int right_operand);
 
-   
+    friend bool operator>=(const Number &left_operand, const Number &right_operand);
+    friend bool operator>=(int left_operand, const Number &right_operand);
+    friend bool operator>=(const Number &left_operand, int right_operand);
+
+    friend bool operator<=(const Number &left_operand, const Number &right_operand);
+    friend bool operator<=(int left_operand, const Number &right_operand);
+    friend bool operator<=(const Number &left_operand, int right_operand);
+
+    friend bool operator!=(const Number &left_operand, const Number &right_operand);
+    friend bool operator!=(int left_operand, const Number &right_operand);
+    friend bool operator!=(const Number &left_operand, int right_operand);
+
 public:
-	Number() = delete;
-    explicit Number(int value );
-          
-	//getter
-	int get_wrapped_int() const{
-            return m_wrapped_int;
-	}
+    Number() = delete;
+    explicit Number(int value);
 
-    auto operator<=>(const Number& right) const = default;
-    auto operator<=> (int n) const{
+    // getter
+    int get_wrapped_int() const
+    {
+        return m_wrapped_int;
+    }
+
+    auto operator<=>(const Number &right) const = default;
+    auto operator<=>(int n) const
+    {
         return m_wrapped_int <=> n;
     }
-    bool operator==(const Number& right) const{
+    bool operator==(const Number &right) const
+    {
         return m_wrapped_int == right.m_wrapped_int;
     }
-    bool operator==(int n){
+    bool operator==(int n)
+    {
         return m_wrapped_int == n;
     }
 
     ~Number();
-private : 
+
+private:
     int m_wrapped_int{0};
 };
-
 
 #endif // NUMBER_H
